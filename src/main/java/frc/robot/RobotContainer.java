c35ed63fdf3a8f07df92b947aa67416882e8bb0a
Erik Frederick <efrederick@control4.com>
2022-11-26 11:23:09 -0500

    added command for shooter, intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,15 @@ package frc.robot;
 import java.lang.ModuleLayer.Controller;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.DriveSubsystem;
 
 /**
@@ -23,12 +28,21 @@ public class RobotContainer {
 
   private DriveSubsystem DRIVE_SUBSYSTEM=new DriveSubsystem();
   private Joystick CONTROLLER=new Joystick(0);
+  private Spark climberMotor = new Spark(3);
+  private Spark intakeMotor = new Spark(5);
+  private Spark shooterMotor = new Spark(6);
+  private Servo shooterServo = new Servo(11);
+
+
+  //private XboxController CONTROLLER=new XboxController(0);
  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
     DRIVE_SUBSYSTEM.setDefaultCommand( new RunCommand(()-> DRIVE_SUBSYSTEM.drive(CONTROLLER.getRawAxis(1), -CONTROLLER.getRawAxis(0)), DRIVE_SUBSYSTEM ) );
+    intakeMotor.setInverted(true);
+    shooterMotor.setInverted(true);
   }
 
   /**
@@ -37,7 +51,26 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    //Climb!
+    JoystickButton a = new JoystickButton(CONTROLLER,1);
+    a.whenPressed(new InstantCommand(() -> climberMotor.set(1))).whenReleased(new InstantCommand(() -> climberMotor.set(0)));
+
+    //Descend!
+    JoystickButton b = new JoystickButton(CONTROLLER,2);
+    b.whenPressed(new InstantCommand(() -> climberMotor.set(-1))).whenReleased(new InstantCommand(() -> climberMotor.set(0)));
+
+    JoystickButton c = new JoystickButton(CONTROLLER,3);
+    c.whenPressed(new InstantCommand(() -> intakeMotor.set(1))).whenReleased(new InstantCommand(() -> intakeMotor.set(0)));
+
+    //Shoot
+    JoystickButton d = new JoystickButton(CONTROLLER,4);
+    d.whenPressed(new InstantCommand(() -> shooterMotor.set(.75))
+          .andThen(new WaitCommand(1))
+          .andThen(new InstantCommand(() -> shooterServo.set(90))))
+      .whenReleased(new InstantCommand(() -> shooterMotor.set(0))
+          .andThen(new InstantCommand(() -> shooterServo.set(0))));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
